4a63e781f67a13422b84f70c0e3e0d594f391dac
James Paul <158149914+james-paul25@users.noreply.github.com>
2024-05-22 19:13:11 +0800

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -4,6 +4,7 @@ function sendMail(event){
     let params = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
         message : document.getElementById("message").value
     }
 
